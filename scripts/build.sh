62cc302f75c71dfc73bbfe72d66bddfa29f0c636
DianQK <dianqk@dianqk.net>
2023-12-09 11:27:10 +0800

    Add --publish=never

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,4 @@
 set -e
 pushd joplin
-yarn workspace @joplin/app-desktop build
-yarn workspace @joplin/app-desktop electron-builder --linux
+yarn workspace @joplin/app-desktop dist --linux --publish=never
 popd
