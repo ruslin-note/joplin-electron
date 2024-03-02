a4d422c962c39731185bcc52b82a70b4864351aa
DianQK <dianqk@dianqk.net>
2024-03-02 22:35:33 +0800

    Update patch.sh

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -3,7 +3,7 @@ cp -r patches ./joplin
 patch -Np1 -i patches/0001-support-enable-wayland-ime.patch -d joplin
 patch -Np1 -i patches/0002-remove-7zip.patch -d joplin
 patch -Np1 -i patches/0003-asar-unpack-node.patch -d joplin
-patch -Np1 -i patches/0004-update-electron.patch -d joplin
+# patch -Np1 -i patches/0004-update-electron.patch -d joplin
 
 sed -i '/"husky": ".*"/d' joplin/package.json
 
