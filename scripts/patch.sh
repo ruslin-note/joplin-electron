af8622e654eb336c250f382e672ecfd1e22426d4
DianQK <dianqk@dianqk.net>
2024-11-23 14:48:31 +0800

    Add back 7zip

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -2,6 +2,5 @@ set -e
 cp -r patches ./joplin
 patch -Np1 -i patches/0001-*.patch -d joplin
 patch -Np1 -i patches/0002-*.patch -d joplin
-patch -Np1 -i patches/0003-*.patch -d joplin
 
 sed -i '/"husky": ".*"/d' joplin/package.json
